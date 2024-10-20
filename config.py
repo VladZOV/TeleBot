7b2e6a5321c44ccf376ad3d5fc4eb095d161f9c5
Vlad Notepad <spider.js.net.ua@mail.ru>
2024-10-20 11:41:00 +1000

    телеграмм-бот(дз)

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '7751138512:AAFFVCwrxY2YW7w--9ec7RdAmeyxEpcmyf4'
+
+keys = {
+    'bitcoin': 'BTC',
+    'efirium': 'ETH',
+    'dollar': 'USD',
+    'rubl': 'RUB',
+    'yen': 'JPY',
+    'euro': 'EUR'
+}
